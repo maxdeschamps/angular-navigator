7251cf95bb7b4643a37e59addd33a3c0baa6dc0c
Sonny <sonnychaprier@gmail.com>
2022-05-10 12:09:38 +0200

    fix: remove console.log in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router, Event, NavigationStart, NavigationEnd, NavigationError} from '@angular/router';
+import { Router, Event, NavigationEnd} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -14,23 +14,11 @@ export class AppComponent {
   constructor(private router: Router) {
     this.currentRoute = "";
     this.router.events.subscribe((event: Event) => {
-      if (event instanceof NavigationStart) {
-        // Show loading indicator
-        console.log('Route change detected');
-      }
 
       if (event instanceof NavigationEnd) {
-        // Hide loading indicator
         this.currentRoute = event.url;
-        console.log(event);
       }
 
-      if (event instanceof NavigationError) {
-        // Hide loading indicator
-
-        // Present error to user
-        console.log(event.error);
-      }
 
     });
   }
